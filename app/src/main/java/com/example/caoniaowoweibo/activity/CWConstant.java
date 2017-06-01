34cc0c5444bd14ef1d9b3bdb77b67ee32d9653ce
yuchengren <271455361@qq.com>
2017-06-01 23:50:06 +0800

    08 新浪SDK未审核

---
+++ app/src/main/java/com/example/caoniaowoweibo/activity/CWConstant.java
@@ -0,0 +1,16 @@
+package com.example.caoniaowoweibo.activity;
+
+/**
+ * Created by Administrator on 2017/6/1.
+ */
+
+public class CWConstant {
+    public static final String APP_KEY      = "1708883998";          // 应用的APP_KEY
+    public static final String SECRET_KEY   ="6cd12ddb9792371487cdde8e5f64a710";
+    public static final String REDIRECT_URL = "http://www.sina.com";// 应用的回调页根据注册的回调页填写
+    public static final String SCOPE =                          // 应用申请的高级权限
+            "email,direct_messages_read,direct_messages_write,"
+                    + "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
+                    + "follow_app_official_microblog," + "invitation_write";
+}
+
--- app/src/main/java/com/example/caoniaowoweibo/activity/LoadingPageActivity.java
+++ app/src/main/java/com/example/caoniaowoweibo/activity/LoadingPageActivity.java
@@ -4,24 +4,61 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.caoniaowoweibo.R;
+import com.sina.weibo.sdk.auth.AuthInfo;
+import com.sina.weibo.sdk.auth.WeiboAuthListener;
+import com.sina.weibo.sdk.auth.sso.SsoHandler;
+import com.sina.weibo.sdk.exception.WeiboException;
+
+import static android.R.attr.data;
 
 public class LoadingPageActivity extends BaseActivity {
+    private SsoHandler mSssoHandler;
+    private AuthInfo mAuthInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getToolBar().hide();//隐藏了登陆页面的ToolBar
-        new Handler().postDelayed(new Runnable() {
+        mAuthInfo = new AuthInfo(getApplicationContext(), CWConstant.APP_KEY,
+                CWConstant.REDIRECT_URL, CWConstant.SCOPE);
+        mSssoHandler = new SsoHandler(this, mAuthInfo);
+        mSssoHandler.authorize(new WeiboAuthListener() {
             @Override
-            public void run() {
-                startActivity(new Intent(LoadingPageActivity.this,HomePageActivity.class));
+            public void onComplete(Bundle bundle) {
+                Log.e("onCreate", bundle + "");
             }
-        },3000);
+
+            @Override
+            public void onWeiboException(WeiboException e) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+        });
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                startActivity(new Intent(LoadingPageActivity.this,HomePageActivity.class));
+//            }
+//        },3000);
     }
 
-    @Override
-    public int getLayoutId() {
-        return R.layout.activity_loading_page;
-    }
+        @Override
+        public int getLayoutId () {
+            return R.layout.activity_loading_page;
+        }
+
+        @Override
+        protected void onActivityResult ( int requestCode, int resultCode, Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+            if (mSssoHandler != null) {
+                mSssoHandler.authorizeCallBack(requestCode, resultCode, data);
+            }
+        }
 }
